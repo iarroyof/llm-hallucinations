32c4cf1d219c38af6540dcbf280934e613137868
Ignacio Arroyo <axayakac@gmail.com>
2025-01-24 13:31:23 -0600

    Update relex_process.py

--- relex_process.py
+++ relex_process.py
@@ -4,11 +4,21 @@ from wiki_sample import WikipediaBatchGenerator
 from pdb import set_trace as st
 
 def extract_relations(answers, n_qs_semantic_search_results, extractor):
+    """
+    results = extractor.extract_relations(texts)
+    for text, relations in results.items():
+        print(f"Text: {text}")
+        for relation in relations:
+            print(f"  Relation: {relation}")
+        print()
+    """
     wiki_docs_fquestion_relations = []
     fanswer_relations = []
     for wiki_docs_from_question, answer in zip(n_qs_semantic_search_results, answers):
-        wiki_docs_fquestion_relations.append(extractor.extraer_relaciones(wiki_docs_from_question))
-        fanswer_relations.append(extractor.extraer_relaciones([answer]))
+        relations = [rs for _, rs in extractor.extract_relations(wiki_docs_from_question).items()]
+        wiki_docs_fquestion_relations.append(relations)
+        relations = [rs for _, rs in extractor.extract_relations([answer]).items()]
+        fanswer_relations.append(relations)
 
     return wiki_docs_fquestion_relations, fanswer_relations
         
