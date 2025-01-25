197f794d2e4cbfc635fedfc46811f2a0f0fd3e1d
Ignacio Arroyo <axayakac@gmail.com>
2025-01-24 19:35:33 -0600

    Update relex_process.py

--- relex_process.py
+++ relex_process.py
@@ -7,9 +7,9 @@ from pdb import set_trace as st
 file_path = 'train/mushroom.en-train_nolabel.v1.jsonl'
 keys = ['model_input', 'model_output_text']
 # Simulate a list of documents from n questions (n_qs),
-# here, each question has associated a batch of documents
+# here, each question qi has associated a batch of m documents
 # n_qs_semantic_search_results = [m_documents_q0, m_documents_q1,..., m_documents_qn]
-# m_documents_q0 = [doc0_from_wiki, doc1_from_wiki,..., docm_from_wiki]
+# m_documents_qi = [doc0_from_wiki, doc1_from_wiki,..., docm_from_wiki]
 generator = WikipediaBatchGenerator()
 n_qs_semantic_search_results = generator.get_batches()
 n_qs = generator.len
