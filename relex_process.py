8be5c3fadd6003b0a2ecac82c040be4ae9da4682
Ignacio Arroyo <axayakac@gmail.com>
2025-01-24 19:30:32 -0600

    Update relex_process.py

--- relex_process.py
+++ relex_process.py
@@ -25,7 +25,10 @@ def extract_relations(answers, n_qs_semantic_search_results, extractor):
 
 file_path = 'train/mushroom.en-train_nolabel.v1.jsonl'
 keys = ['model_input', 'model_output_text']
-# Simulate a list of documents from n questions (n_qs)
+# Simulate a list of documents from n questions (n_qs),
+# here, each question has associated a batch of documents
+# n_qs_semantic_search_results = [m_documents_q0, m_documents_q1,..., m_documents_qn]
+# m_documents_q0 = [doc0_from_wiki, doc1_from_wiki,..., docm_from_wiki]
 generator = WikipediaBatchGenerator()
 n_qs_semantic_search_results = generator.get_batches()
 n_qs = generator.len
